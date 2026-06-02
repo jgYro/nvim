66964fa04bc9549d8f093e876feff387ec75d6bd
Jericho Gregory <j.yrogerg.programs@gmail.com>
2026-06-02 17:03:58 -0400

    Updated telescope keybinds

--- lua/plugin_config/telescope.lua
+++ lua/plugin_config/telescope.lua
@@ -28,6 +28,13 @@ vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Open buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
 vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Recent files" })
 vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "Resume last picker" })
+vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Find diagnostics" })
+vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "Git branches" })
+vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Git commits" })
+vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git status" })
+vim.keymap.set("n", "<leader>gl", builtin.git_bcommits, { desc = "Git buffer commits" })
+vim.keymap.set("n", "<leader>gL", builtin.git_bcommits_range, { desc = "Git buffer commits (range)" })
+vim.keymap.set("n", "<leader>km", builtin.keymaps, { desc = "Keymaps" })
 
 -- Group label for the <Space>f prefix in the which-key popup. pcall keeps
 -- this file independent of whether which-key is installed.
